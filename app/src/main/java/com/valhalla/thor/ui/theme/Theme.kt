371479530b8005908356739fd944e06cc596ba02
trinadh thatakula <trinadh.thatakula@gmail.com>
2025-01-26 19:01:00 +0530

    feat: Update app font and use downloadable fonts
    This commit updates the app font to "Fira Mono" and "Outfit" and uses downloadable fonts from Google Fonts.
    
    - Adds font certificates for Google Fonts.
    - Updates the typography to use the new fonts.
    - Adds the "Fira Mono" font file to the resources.
    - Updates the `Theme.kt` file to use the new `AppTypography`.

--- app/src/main/java/com/valhalla/thor/ui/theme/Theme.kt
+++ app/src/main/java/com/valhalla/thor/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.valhalla.thor.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -52,7 +51,7 @@ fun ThorTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = AppTypography,
         content = content
     )
 }
