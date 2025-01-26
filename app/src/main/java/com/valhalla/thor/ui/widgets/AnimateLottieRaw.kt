b8e5678393c18302bb8b654418c140638bdb9a9f
trinadh thatakula <trinadh.thatakula@gmail.com>
2025-01-26 19:02:13 +0530

    feat: Add ContentScale parameter to AnimateLottieRaw composable
    This commit adds a new parameter `contentScale` to the `AnimateLottieRaw` composable function. This allows developers to control how the Lottie animation is scaled to fit the available space.
    
    The default value for `contentScale` is `ContentScale.None`.

--- app/src/main/java/com/valhalla/thor/ui/widgets/AnimateLottieRaw.kt
+++ app/src/main/java/com/valhalla/thor/ui/widgets/AnimateLottieRaw.kt
@@ -4,6 +4,7 @@ import androidx.annotation.RawRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -15,7 +16,8 @@ fun AnimateLottieRaw(
     modifier: Modifier = Modifier,
     @RawRes resId: Int,
     shouldLoop: Boolean = false,
-    repeatCount: Int = LottieConstants.IterateForever
+    repeatCount: Int = LottieConstants.IterateForever,
+    contentScale: ContentScale = ContentScale.None
 ) {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(resId))
     val progress by animateLottieCompositionAsState(
@@ -26,5 +28,6 @@ fun AnimateLottieRaw(
         composition = composition,
         progress = { progress },
         modifier = modifier,
+        contentScale = contentScale
     )
 }
