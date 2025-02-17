5bbf1559134e6983415aa9b0e16c0b9799f1c752
trinadh thatakula <trinadh.thatakula@gmail.com>
2025-02-17 19:46:00 +0530

    chore: Bump version to 1.326
    This commit updates the app version:
    - versionCode is incremented to 1326
    - versionName is updated to 1.326
    - Update `app-release.dm` files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.valhalla.thor"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1325
-        versionName = "1.325"
+        versionCode = 1326
+        versionName = "1.326"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val p = Properties()
