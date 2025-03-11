e885298cd277a7a0827a51927cf2a8bc0eec480f
Trinadh thatakula <trinadh.thatakula@gmail.com>
2025-03-11 14:56:39 +0530

    chore: Update app version to 1.501
    This commit updates the app's version:
    
    -   `versionCode` is incremented to 1501.
    -   `versionName` is updated to 1.501.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.valhalla.thor"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1500
-        versionName = "1.500"
+        versionCode = 1501
+        versionName = "1.501"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val p = Properties()
