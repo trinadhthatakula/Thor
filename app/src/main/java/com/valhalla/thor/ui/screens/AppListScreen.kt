053cbaab091b177c68fada87371eafd3d44a6578
trinadh thatakula <trinadh.thatakula@gmail.com>
2025-06-22 11:26:22 +0530

    Refactor: Update Baseline Profile and AppListScreen
    This commit updates the baseline profiles and makes a minor adjustment to `AppListScreen.kt`.
    
    - Updated `app/src/release/generated/baselineProfiles/baseline-prof.txt` and `app/src/release/generated/baselineProfiles/startup-prof.txt` with numerous changes across various classes, including `androidx.compose.runtime.saveable`, `androidx.compose.ui.node`, `androidx.emoji2.text`, `com.valhalla.superuser`, and `com.valhalla.thor.ui.home`.
    - In `app/src/main/java/com/valhalla/thor/ui/screens/AppListScreen.kt`, replaced `import androidx.compose.material3.CircularProgressIndicator` with `import androidx.compose.material3.ContainedLoadingIndicator`.

--- app/src/main/java/com/valhalla/thor/ui/screens/AppListScreen.kt
+++ app/src/main/java/com/valhalla/thor/ui/screens/AppListScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ContainedLoadingIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
