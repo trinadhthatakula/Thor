2f0fdcb6512a9a3748825ecaee63b27724a87391
trinadhthatakula <trinadh.thatakula@gmail.com>
2026-06-01 21:43:44 +0530

    feat(navigation): create type-safe serializable ThorRoute keys for Navigation 3

---
+++ app/src/main/java/com/valhalla/thor/presentation/navigation/ThorRoute.kt
@@ -0,0 +1,23 @@
+package com.valhalla.thor.presentation.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface ThorRoute : NavKey {
+
+    @Serializable
+    data object Home : ThorRoute
+
+    @Serializable
+    data object Apps : ThorRoute
+
+    @Serializable
+    data object Freezer : ThorRoute
+
+    @Serializable
+    data object Settings : ThorRoute
+
+    @Serializable
+    data class PermissionManager(val packageName: String, val appName: String) : ThorRoute
+}
